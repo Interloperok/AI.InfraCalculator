ecad7b3c093e5357f48bc4576420bb23dee1c3f1
Pavel Sozonov <pavel.sozonov@gmail.com>
2026-02-22 14:27:53 -0800

    Switch to using 'docker compose' command

--- redeploy.sh
+++ redeploy.sh
@@ -3,12 +3,12 @@
 set -e
 
 echo "Building new images..."
-docker-compose -f docker-compose.prod.yml build
+docker compose -f docker-compose.prod.yml build
 
 echo "Stopping and removing old containers..."
-docker-compose -f docker-compose.prod.yml down
+docker compose -f docker-compose.prod.yml down
 
 echo "Starting new containers with freshly built images..."
-docker-compose -f docker-compose.prod.yml up -d
+docker compose -f docker-compose.prod.yml up -d
 
 echo "Deployment completed successfully!"
